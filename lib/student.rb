c950fa407ecaceb5d9e10cf3db1c412c216dbab9
uu <giladbarkan@live.com>
2016-06-28 11:29:58 -0400

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-   self.column_names.each{|col| attr_accessor col.to_sym}
+  self.column_names.each{|col| attr_accessor col.to_sym}
 
   def initialize(options=nil)
    if options
