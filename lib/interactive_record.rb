fdc6b8e12713460d89acc5ddfa3db8f21ea78409
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-08-14 17:09:03 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,6 +29,10 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
+    # value = attribute_hash.values.first
+    # formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    # sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    # DB[:conn].execute(sql)
     hash = []
     if attribute[:grade]
       sql = <<-SQL
